3b8964885faa6fcdf3825b7cfe51cab78ffbd433
cacaprog <ccananea@yahoo.com.br>
2025-01-18 22:13:08 -0300

    upload module 1 files and hw

---
+++ 1_intro/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import pandas
+
+# print arguments
+print(sys.argv)
+
+# argument 0 is the name os the file
+# argument 1 is the first argument
+day = sys.argv[1]
+
+# cool pandas stuff here
+# print a sentence with the argument
+print(f"job successfully finished for day = {day}")
